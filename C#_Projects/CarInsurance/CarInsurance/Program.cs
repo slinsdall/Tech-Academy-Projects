dd412ecacf260806e30de56d98c877b4e24f105f
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-10-24 13:32:13 -0700

    Update

--- C#_Projects/CarInsurance/CarInsurance/Program.cs
+++ C#_Projects/CarInsurance/CarInsurance/Program.cs
@@ -10,32 +10,32 @@ namespace CarInsurance
     {
         static void Main()
         {
-            Console.WriteLine("Car Insurance Approval Program");
+            Console.WriteLine("Car Insurance Approval Program");//This is just the title of the program
             Console.ReadLine();
 
             Console.WriteLine("What is your age?");
-            int Age = Convert.ToInt32(Console.ReadLine()); //This will save the data inout
+            int Age = Convert.ToInt32(Console.ReadLine()); //This will save the data input
 
 
             Console.WriteLine("Have you ever had a DUI? \nPlease answer true or false");
-            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            bool DUI = Convert.ToBoolean(Console.ReadLine());//This will save the input data
 
 
             Console.WriteLine("How many speeding tickets do you have?");
-            int Tickets = Convert.ToInt32(Console.ReadLine());
+            int Tickets = Convert.ToInt32(Console.ReadLine());//This saves the input data
 
   
 
             Console.WriteLine("Qualified?");
             Console.ReadLine();
-            if (Age >= 15 && DUI == false && Tickets <= 3)
+            if (Age >= 15 && DUI == false && Tickets <= 3) // This will specify the qualification points
             {
-                Console.WriteLine("True");
+                Console.WriteLine("True");//If the qualification specifities are met it will show true
                 Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("False");
+                Console.WriteLine("False");//If the qualification specifities are not met it will show false
                 Console.ReadLine();
             }
           
