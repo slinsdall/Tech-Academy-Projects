e48fb82dc9cd52b9f559c7214be8ce72ee0a1a12
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-10-25 11:00:38 -0700

    Update

--- C#_Projects/PackageExpress/PackageExpress/Program.cs
+++ C#_Projects/PackageExpress/PackageExpress/Program.cs
@@ -26,43 +26,37 @@ namespace PackageExpress
                 Console.WriteLine("What is the package width?");
                 int width = Convert.ToInt32(Console.ReadLine());
 
-                if (width > 50)//If statements will kick in if user input is above 50
-                {
-                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-                    Console.ReadLine();
-                }
+               
 
                 Console.WriteLine("What is the package height?");
                 int height = Convert.ToInt32(Console.ReadLine());
 
-                if (height > 50)//If statements will kick in if user input is above 50
-                {
-                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-                    Console.ReadLine();
-                }
+                
 
                 Console.WriteLine("What is the package length?");
                 int length = Convert.ToInt32(Console.ReadLine());
 
-                if (length > 50)//If statements will kick in if user input is above 50
+                if (width + height + length > 50)
                 {
-                    Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
                     Console.ReadLine();
                 }
+                else
+                {
 
-                int dimensions = width * height * length;//This will multiply all dimensions
+                    int dimensions = width * height * length;//This will multiply all dimensions
 
-                int total = dimensions * weight;//Takes the dimension after multiplication and mutiplies by weight
+                    int total = dimensions * weight;//Takes the dimension after multiplication and mutiplies by weight
 
-                decimal outcome = total / 100;//Takes the total and divides by 100
+                    decimal outcome = total / 100;//Takes the total and divides by 100
 
-                Console.WriteLine("Your estimated total for shipping this package is: $" + outcome);//This will add the sentance with the outcome at the end
-                Console.ReadLine();
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + outcome);//This will add the sentance with the outcome at the end
+                    Console.ReadLine();
 
-                Console.WriteLine("Thank you!");
-                Console.ReadLine();
-                
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
 
+                }
 
 
 
