277b54f5539ed7ce7b5b2974a684fb03aa0fc7fd
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-10-26 14:49:45 -0700

    Update

---
+++ C#_Projects/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sarah";
+            string car = "Subaru";
+            string quote = "Hello, Sarah. You drive a Subaru.";
+
+            name = name.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Sarah \n and I drive a subaru \n Thank you.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
