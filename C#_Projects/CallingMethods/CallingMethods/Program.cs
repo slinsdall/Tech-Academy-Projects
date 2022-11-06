ae1c0a1f2a399a946cbb51fe057d7da65036f626
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-06 15:15:53 -0800

    Update

--- C#_Projects/CallingMethods/CallingMethods/Program.cs
+++ C#_Projects/CallingMethods/CallingMethods/Program.cs
@@ -4,23 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CallingMethods
-{
+
     class Program
     {
         static void Main(string[] args)
         {
-            Math math = new Math();
-            math add(Math);
+            Maths maths = new Maths();
 
-            foreach(Math math in math.Maths)
-            {
-                Console.WriteLine("Please pick a number: ");
-                int selection = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.WriteLine(math.Maths.Count);
+
+        foreach (Math math in math.Maths)
+        {
+            Console.WriteLine("Please pick a number: ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.add(selection));
             Console.ReadLine();
+
+            Console.WriteLine(math.subtract(selection));
+            Console.ReadLine();
+
+            Console.WriteLine(math.divide(selection));
+            Console.ReadLine();
+        }
         }
         
     }
-}
+
