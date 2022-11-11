c14ad0df765e4bcff26191f4d02fdb53a84547af
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-10 18:08:18 -0800

    Update

--- C#_Projects/MethodSubmission/MethodSubmission/Program.cs
+++ C#_Projects/MethodSubmission/MethodSubmission/Program.cs
@@ -16,34 +16,29 @@ namespace MethodSubmission
             Console.WriteLine("Please put in 2 numbers one at a time,\n Please type the first number: ");
             int input = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("You do not need to enter a second number. if you would like to add a second number type here: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(method.Math(input));
-            Console.ReadLine();
-
             try
             {
-                Console.WriteLine("If you would like to add a second number type here: ");
-                int secondnum = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine(method.Math(secondnum));
-                Console.ReadLine();
+                Console.WriteLine("You do not need to enter a second number. if you would like to add a second number type here: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                
+               
+
+            
+                Console.WriteLine("If you would like to add a second number type here: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(method.Math(num2));
+                
 
             }
             catch (FormatException ex)
             {
-                Console.WriteLine("Please type a whole number.");
+                Console.WriteLine(method.Math(input));
                 return;
             }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Please do not enter 0");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            
             finally
             {
                 Console.ReadLine();
