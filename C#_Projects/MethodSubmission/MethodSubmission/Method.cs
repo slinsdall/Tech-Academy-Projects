48b1d96ef390f0a78acfd22e710932bb9a336baf
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-10 16:16:32 -0800

    Upload

---
+++ C#_Projects/MethodSubmission/MethodSubmission/Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Method
+    {
+        public  int Math(int a, int b)
+        {
+            int equation = a + 7;
+            int optional = b * 26;
+            return equation;
+        }
+        
+    }
+}
---
+++ C#_Projects/MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Method method = new Method();
+
+            Console.WriteLine("Please put in 2 numbers one at a time,\n Please type the first number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You do not need to enter a second number.");
+            Console.ReadLine();
+
+            Console.WriteLine(method.Math(input));
+            Console.ReadLine();
+        }
+    }
+}
