66f343aa6ce0d0a983f269df064697d80e4cfc38
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-13 11:27:56 -0800

    Update

---
+++ C#_Projects/MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person
+    {
+        List<string> Persons { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void  SayName()
+        {
+            foreach (string person in Persons)
+            {
+                Console.WriteLine(person);
+                
+            }
+        }
+    }
+}
---
+++ C#_Projects/MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Person person = new Person();
+            Employee employee = new Employee();
+            employee.Persons = new List<string>() { "Sample", "Student" };
+            employee.SayName();
+            employee.ID();
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
