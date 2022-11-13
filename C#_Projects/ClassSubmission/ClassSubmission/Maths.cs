290d4e11d35cab99ba23ad9425b99ebf8cb1e61a
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-13 10:23:06 -0800

    Update

---
+++ C#_Projects/ClassSubmission/ClassSubmission/Maths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public static class Maths
+    {
+        public void division(int E)
+        {
+            int equation = E / 2;
+            
+        }
+
+        public static int division(int H)
+        {
+            int result = H / 5;
+            return result;
+        }
+        
+    }
+}
---
+++ C#_Projects/ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths math = new Maths();
+
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.division(input));
+            Console.ReadLine();
+        }
+    }
+}
