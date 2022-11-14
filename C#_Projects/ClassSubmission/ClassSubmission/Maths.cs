c1269c374db6a996e4beef17ff9620044d10f39b
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-14 15:47:24 -0800

    Update

--- C#_Projects/ClassSubmission/ClassSubmission/Maths.cs
+++ C#_Projects/ClassSubmission/ClassSubmission/Maths.cs
@@ -6,18 +6,28 @@ using System.Threading.Tasks;
 
 namespace ClassSubmission
 {
-    public static class Maths
+    public  class Maths
     {
+        public int problem { get; set; }
+
         public void division(int E)
         {
             int equation = E / 2;
             
         }
 
-        public static int division(int H)
+        public int division(int H)
         {
             int result = H / 5;
             return result;
+        }
+
+        public static int ADD(int L, out int add)
+        {
+            add = 0;
+            total = ADD(out add, 5);
+            int total = L + 9;
+            return total;
         }
         
     }
