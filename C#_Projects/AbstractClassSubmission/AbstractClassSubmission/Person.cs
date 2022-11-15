d88ecfce39e8a0cc3cb92eea7a752b024fabe98f
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-15 15:50:00 -0800

    Update

---
+++ C#_Projects/AbstractClassSubmission/AbstractClassSubmission/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public  virtual string SayName()
+        {
+            Console.WriteLine(firstName + lastName);
+            
+        }
+    }
+}
