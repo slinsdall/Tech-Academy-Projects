234b2ebeef62e585fb30ffe293b6808c6bde0f87
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-15 15:12:38 -0800

    Update

--- C#_Projects/ClassSubmission/ClassSubmission/Maths.cs
+++ C#_Projects/ClassSubmission/ClassSubmission/Maths.cs
@@ -9,6 +9,7 @@ namespace ClassSubmission
     public  class Maths
     {
         public int problem { get; set; }
+        public int equation { get; set; }
 
         public void division(int E)
         {
@@ -18,15 +19,16 @@ namespace ClassSubmission
 
         public int division(int H)
         {
-            int result = H / 5;
+            int result = H * 5;
             return result;
         }
-
+       
         public static int ADD(int L, out int add)
         {
-            add = 0;
-            total = ADD(out add, 5);
+            
             int total = L + 9;
+            total = add(out add, out L, 5);
+            
             return total;
         }
         
---
+++ C#_Projects/ClassSubmission/ClassSubmission/Static.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public static class Static
+    {
+        public string subtract(int S)
+        {
+            int results = S - 5;
+            return results;
+        }
+    }
+}
