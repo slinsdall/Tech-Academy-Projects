102aa903fe1aeed65244f2451992aafaa79cd0c2
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-15 16:09:32 -0800

    Update

---
+++ C#_Projects/PolymorphismSumbission/PolymorphismSumbission/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSumbission
+{
+     public class Employee : IQuittable 
+    {
+        public List<string> Person { get; set; }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#_Projects/PolymorphismSumbission/PolymorphismSumbission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSumbission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Person = new List<String>() { "Sarah", "Kirsten", "Nicole" };
+
+        }
+    }
+}
