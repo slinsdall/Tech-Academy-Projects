9c0444928812f708362995fcfcd43901080a6ffa
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-17 14:57:02 -0800

    update

---
+++ C#_Projects/OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Employee 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ID { get; set; }
+
+        public static Employee operator==(Employee employee)
+        {
+            employee.Add();
+            return employee;
+        }
+        public static Employee operator!=(Employee employee)
+        {
+            employee.Add();
+            return employee;
+        }
+
+
+    }   
+}
