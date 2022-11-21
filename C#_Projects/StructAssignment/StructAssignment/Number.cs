dcebe71f0e5635d1125d34b0ab0839d03fdd3a38
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-21 15:59:06 -0800

    Update

---
+++ C#_Projects/StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number//Create a struct called Number and give it the property “Amount” and have it be of data type decimal.
+
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ C#_Projects/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+
+            number.Amount = 5;//In the Main() method, create an object of data type Number and assign an amount to it.
+
+            Console.WriteLine(number.Amount);//Print this amount to the console.
+            Console.ReadLine();
+        }
+    }
+}
