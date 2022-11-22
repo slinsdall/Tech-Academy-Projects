79824eacceb70013e50d52f57fed66376d5e3c97
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-22 15:37:50 -0800

    update

--- C#_Projects/LambdaSubmission/LambdaSubmission/Employee.cs
+++ C#_Projects/LambdaSubmission/LambdaSubmission/Employee.cs
@@ -8,5 +8,14 @@ namespace LambdaSubmission
 {
     class Employee
     {
+        //Create an Employee class with the following properties:
+        //a.Id
+        //b.First Name
+        //c. Last Name
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+
     }
 }
--- C#_Projects/LambdaSubmission/LambdaSubmission/Program.cs
+++ C#_Projects/LambdaSubmission/LambdaSubmission/Program.cs
@@ -10,6 +10,20 @@ namespace LambdaSubmission
     {
         static void Main(string[] args)
         {
+            //In the Main() method, create a list of at least 10 employees. 
+            //At least two employees should have the first name “Joe”.
+            Employee employee = new Employee();
+
+            List<string> employees = new List<string>() { "Sarah", "Joe", "Nicole", "Kirsten", "Joe", "Aaron", "Carys", "Ryan", "Luna", "Emma" };
+
+            List<Employee> newList = new List<Employee>();
+            //Using a foreach loop, create a new list of all employees with the first name “Joe”. 
+            //In your comparison statement, remember to reference the property of the object you are checking.
+            foreach(Employee employees2 in newList)
+            {
+
+            }
+
         }
     }
 }
