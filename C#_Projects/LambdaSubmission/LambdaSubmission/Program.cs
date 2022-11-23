40bc1706a27e19db8c8303d240c12d32e978656e
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-23 15:54:24 -0800

    Update

--- C#_Projects/LambdaSubmission/LambdaSubmission/Program.cs
+++ C#_Projects/LambdaSubmission/LambdaSubmission/Program.cs
@@ -26,16 +26,16 @@ namespace LambdaSubmission
             }
 
             //Perform the same action again, but this time with a lambda expression.
-            List<Employee> newList2 = new List<Employee>() { new Employee() { ID = 1, FirstName = "Sarah", LastName = "Keating" }, new Employee() { ID = 2, FirstName = "Kirsten", LastName = "Keating" }, new Employee() { ID = 3, FirstName = "Joe", LastName = "Luscombe" }, new Employee() { ID = 4, FirstName = "Nicole", LastName = "Hart" }, new Employee() { ID = 5, FirstName = "Joe", LastName = "Smith" }, new Employee() { ID = 6, FirstName = "Aaron", LastName = "Chandler" }, new Employee() { ID = 7, FirstName = "Emma", LastName = "Arnts" }, new Employee() { ID = 8, FirstName = "Rachel", LastName = "Willis" }, new Employee() { ID = 9, FirstName = "Leah", LastName = "Jordan" }, new Employee() { ID = 10, FirstName = "Carys", LastName = "Mills" } };
-            string count = newList2.employees.Count(x => x.FirstName == FirstName.Joe).ToList();
+            List<Employee> joes2 = newList.Where(x => x.FirstName == "Joe").ToList();
+            
 
-            foreach (Employee employees in newList2)
+            foreach (Employee employee1 in joes2)
             {
-                Console.WriteLine(count);
+                Console.WriteLine(joes2);
                 Console.ReadLine();
             }
             //Using a lambda expression, make a list of all employees with an Id number greater than 5
-            int numbID = newList2.Where(ID => ID < 5).ToList;
+            List<Employee> numbID = newList.Where(x => x.ID < 5).ToList();
             Console.WriteLine(numbID);
             Console.ReadLine();
 
