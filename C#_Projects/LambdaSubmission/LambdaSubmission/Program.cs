a090363c486c7bf8e45c91b993b1a71cad7ae78d
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-23 18:01:44 -0800

    update

--- C#_Projects/LambdaSubmission/LambdaSubmission/Program.cs
+++ C#_Projects/LambdaSubmission/LambdaSubmission/Program.cs
@@ -19,10 +19,13 @@ namespace LambdaSubmission
             List<Employee> newList = new List<Employee>() { new Employee() { ID = 1, FirstName = "Sarah", LastName = "Keating" }, new Employee() { ID = 2, FirstName = "Kirsten", LastName = "Keating" }, new Employee() { ID = 3, FirstName = "Joe", LastName = "Luscombe" }, new Employee() { ID = 4, FirstName = "Nicole", LastName = "Hart" }, new Employee() { ID = 5, FirstName = "Joe", LastName = "Smith" }, new Employee() { ID = 6, FirstName = "Aaron", LastName = "Chandler" }, new Employee() { ID = 7, FirstName = "Emma", LastName = "Arnts" }, new Employee() { ID = 8, FirstName = "Rachel", LastName = "Willis" }, new Employee() { ID = 9, FirstName = "Leah", LastName = "Jordan" }, new Employee() { ID = 10, FirstName = "Carys", LastName = "Mills" } };
             //Using a foreach loop, create a new list of all employees with the first name “Joe”. 
             //In your comparison statement, remember to reference the property of the object you are checking.
+            List<Employee> joeList = new List<Employee>();
             foreach(Employee employees2 in newList)
             {
-                Console.WriteLine(employees2.FirstName == "Joe");
-                Console.ReadLine();
+                if (employees2.FirstName == "Joe")
+                {
+                    joeList.Add(employees2);
+                }
             }
 
             //Perform the same action again, but this time with a lambda expression.
@@ -31,7 +34,7 @@ namespace LambdaSubmission
 
             foreach (Employee employee1 in joes2)
             {
-                Console.WriteLine(joes2);
+                Console.WriteLine(employee1.FirstName +  " "  + employee1.LastName);
                 Console.ReadLine();
             }
             //Using a lambda expression, make a list of all employees with an Id number greater than 5
