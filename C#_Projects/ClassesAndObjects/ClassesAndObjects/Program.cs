b18075b1f493c249a98d0648d3d9994944a5cac7
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-24 12:23:56 -0800

    update

---
+++ C#_Projects/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name?");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}. Would you like to join a game of Twenty One?", playerName);
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while(player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now!");
+            Console.Read();
+        }
+        
+        
+       
+    }
+}
