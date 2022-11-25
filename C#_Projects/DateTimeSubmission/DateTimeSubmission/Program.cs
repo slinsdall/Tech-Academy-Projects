1d0b6bdc39636d47c4e6dda2d88f8b8300c10ea2
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-25 15:00:59 -0800

    Update

---
+++ C#_Projects/DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Prints the current date and time to the console.
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+            //Asks the user for a number.
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Prints to the console the exact time it will be in X hours, 
+            //X being the number the user entered in step 2.
+            DateTime currentTime = DateTime.Now;
+            DateTime userInput = currentTime.AddHours(input);
+            Console.WriteLine(userInput);
+            Console.ReadLine();
+        }
+    }
+}
