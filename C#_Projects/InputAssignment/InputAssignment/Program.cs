2623b9dbc7a085f8bc0e74ace86e0c097d4c82e6
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-25 14:18:00 -0800

    update

---
+++ C#_Projects/InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the user for a number.
+            Console.WriteLine("Please enter a number: ");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            //Logs that number to a text file.
+            using (StreamWriter file = new StreamWriter(@"C:\Users\slins\Logs\log.txt", true))
+            {
+                file.WriteLine(inputNum);
+            }
+            //Prints the text file back to the user.
+            Console.WriteLine(inputNum);
+            Console.ReadLine();
+
+        }
+    }
+}
