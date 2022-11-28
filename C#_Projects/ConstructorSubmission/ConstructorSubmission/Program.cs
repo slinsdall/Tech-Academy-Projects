457ff2cf0a443f1c6b5bb2ac6e06fe7a3005846d
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-28 14:30:56 -0800

    Update

---
+++ C#_Projects/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using Google.Apis.Admin.Directory.directory_v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a const variable.
+            const string FirstName = "Sarah";
+            //Create a variable using the keyword “var.”
+            var LastName = "Linsdall";
+
+            //Chain two constructors together
+           
+        }
+
+
+        
+    }
+}
