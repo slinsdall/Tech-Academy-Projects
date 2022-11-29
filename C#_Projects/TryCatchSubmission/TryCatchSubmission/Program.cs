5444b39ef162ec5c6ff4eab953322f3b141cba3c
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-11-29 13:54:28 -0800

    update

--- C#_Projects/TryCatchSubmission/TryCatchSubmission/Program.cs
+++ C#_Projects/TryCatchSubmission/TryCatchSubmission/Program.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace TryCatchSubmission
 {
     class Program
     {
+        public static DateTime current { get; private set; }
+
         static void Main(string[] args)
         {
             //Ask the user for their age.
             Console.WriteLine("Please enter your age: ");
             int userAge = Convert.ToInt32(Console.ReadLine());
             //Display the year the user was born.
-            DateTime now = DateTime.Now.Year;
-            now = now.AddYears(-userAge);
-            
-            Console.WriteLine(now);
-            Console.ReadLine();
+            DateTime now = DateTime.Now;
 
+            //Exceptions must be handled using “try/catch.
+             
+                try
+                {
+                    Console.WriteLine(now.Year - userAge);
+                    Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please do not enter zero or negative numbers.");
+                    return;
+                }
+                catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return;
+                    }
+                finally
+                {
+                    Console.ReadLine();
+
+                }
+
+            
 
             
 
